83e70e73cdf99865decf3984fd23c3089ec4be43
Natan <natan.santolo@gmail.com>
2011-11-15 11:03:53 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var fs = require('fs'),
+	util   = require('util'),
+	express = require('express'),
+	io = require('socket.io'),
+	port = process.argv[2] || 8080;
+
+/**
+* app & basic data
+*/
+var app = express.createServer(),
+	data = { 
+		title: 'Observer', 
+		urls: [],
+		results: []
+	 };
+
+/**
+* app configuration.
+*/
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler()); 
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+
+/**
+* Routing
+*/
+app.get('/', function(req, res, next){
+	// render
+	data.ip = req.connection.remoteAddress;
+	res.render('index', data );	
+});
+
+/**
+* app start
+*/
+app.listen(port);
+
+/**
+* Socket.IO.JS Magic
+*/
+var io = io.listen(app);
+
+io.sockets.on('connection', function (socket) {
+
+	/**
+	* Overview socket listener
+	*/
+	socket.on('connected', function(_id) {
+		socket.broadcast.emit('create pointer', { _id: _id });
+	});
+	
+	socket.on('moving', function(_id, data) {
+		socket.broadcast.emit('user moving', { _id: _id, data: data });
+	});
+
+	socket.on('clicking', function(_id, data, event) {
+		socket.broadcast.emit('clicked', { _id: _id, data: data, event: event });
+	});
+
+/**
+* End Socket
+*/
+});
+
+
+
+/**
+* log
+*/
+console.log("Phantomland listening on port %d", app.address().port);
---
+++ public/js/observer.js
@@ -0,0 +1,155 @@
+
+/**
+ *	Namespace
+ */
+
+var o = {};
+	o.pointers = {};
+
+/**
+ *	Cookies
+ */
+//	$.cookie('pointer',null);
+	
+	if ( $.cookie('pointer') ) {
+		o._id = $.cookie('pointer');
+	} else {
+		o._id = 'pointer' + Math.floor(Math.random()*16777215).toString(16);
+		$.cookie('pointer', o._id);
+	}
+
+/**
+ *	Utils
+ */
+var getMousePosition = function(e) {
+
+	// From http://www.quirksmode.org/js/events_properties.html
+
+	var posx = 0;
+	var posy = 0;
+
+	if (!e) var e = window.event;
+
+	if (e.pageX || e.pageY) {
+		posx = e.pageX;
+		posy = e.pageY;
+	}
+	else if (e.clientX || e.clientY) {
+		posx = e.clientX + document.body.scrollLeft
+			+ document.documentElement.scrollLeft;
+		posy = e.clientY + document.body.scrollTop
+			+ document.documentElement.scrollTop;
+	}
+
+	return { 
+		posx: posx,
+		posy: posy
+	}
+}
+
+/**
+ *	Controls
+ */
+
+var createPointerControl =  function(e) {
+
+	var id = e._id;
+
+	if ( o.pointers[id] ) {
+		return;
+	}
+
+	o.pointers[id] = $('<div id="' + id + '">')
+		.css({
+			'background-color': '#' + id.split('pointer').join(''),
+			'width': '5px',
+			'height': '5px',
+			'position': 'absolute'
+		})
+		.addClass('pointer')
+		.appendTo('body');
+};
+
+var userMovingControl = function(e) {
+
+		var id = e._id, w, h,
+			posx = e.data.posx,
+			posy = e.data.posy;
+
+		if ( !o.pointers[id] ) {
+			return;
+		}
+
+		w = o.pointers[id].css('width').split('px').join('');
+		h = o.pointers[id].css('height').split('px').join('');
+
+		o.pointers[id].css({
+			'top': posy - h + 'px',
+			'left': posx - w + 'px'
+		});
+};
+
+var clickedControl = function(e) {
+console.log('clicek pointer')
+
+	var id = e._id, w, h,
+		posx = e.data.posx,
+		posy = e.data.posy
+		type = e.event;
+console.log(type)
+	if ( !o.pointers[id] ) {
+		return;
+	}
+
+	w = o.pointers[id].css('width').split('px').join('');
+	h = o.pointers[id].css('height').split('px').join('');
+
+	if (type === 'mousedown') {
+		o.pointers[id].addClass('clicked').css({
+			'width': '10px',
+			'height': '10px',
+			'top': posy - (h/2) + 'px',
+			'left': posx - (w/2) + 'px'
+		}).removeClass('clicked');
+	} else {
+		o.pointers[id].css({
+			'width': '5px',
+			'height': '5px',
+			'top': posy + 'px',
+			'left': posx + 'px'
+		});
+	}
+};
+
+/**
+ *	Sockets
+ */
+	o.socket = io.connect('http://10.65.2.1:8080');
+
+	// Connected
+	o.socket.on('connect', function (e) {
+    	o.socket.emit('connected', o._id);
+	});
+
+	o.socket.on('create pointer', createPointerControl);
+
+	o.socket.on('user moving', userMovingControl);
+
+	o.socket.on('clicked', clickedControl);
+
+
+
+/**
+ *	Events
+ */
+
+// Mouse move event
+$(window).mousemove(function(e) {
+	o.socket.emit("moving", o._id, getMousePosition(e) );
+});
+// Mouse click
+var clicking = function(e) {
+	o.socket.emit("clicking", o._id, getMousePosition(e), e.type );
+};
+// Mousedown // Mouseup
+$(window).mousedown(clicking).mouseup(clicking);
